1e8ad364a1051d9b9652495ce11ba54ab2e745e2
adrian1lara <adrianlara203@gmail.com>
2023-10-19 18:48:42 -0500

    removeFromcart prop added, and Nav component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,8 +2,9 @@ import useData from "../services/useData"
 import ProductList from "../components/ProductList"
 import propTypes from "prop-types"
 import CartList from "../components/CartList"
+import Nav from "../components/Nav"
 
-const Products = ({ addToCart, cart}) => {
+const Products = ({ addToCart, cart, removeFromCart }) => {
 
     const { data } = useData(`https://fakestoreapi.com/products`)
     
@@ -14,16 +15,18 @@ const Products = ({ addToCart, cart}) => {
 
     return (
         <>
+            <Nav />
             <h1>Products</h1>
             <ProductList data={data} addToCart={addToCart} />
-            <CartList cart={cart} />
+            <CartList cart={cart}  addToCart={addToCart} removeFromCart={removeFromCart}/>
         </>
     )
 }
 
 Products.propTypes = {
     addToCart: propTypes.func,
-    cart: propTypes.array
+    cart: propTypes.array,
+    removeFromCart: propTypes.func
 }
 
 
