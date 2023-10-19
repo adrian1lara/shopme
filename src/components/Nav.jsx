aaa70aa4d9080dd14b958db02ff254a74b66fcd1
adrian1lara <adrianlara203@gmail.com>
2023-10-19 17:54:05 -0500

    links for router

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+const Nav = () => {
+    return (
+        <>
+            <h1>Shopme</h1>
+            <nav>
+                <li>
+                    <a href="/">Home</a>
+                    <a href="/products">Products</a>
+                </li>
+            </nav>
+        </>
+    )
+}
+
+export default Nav
