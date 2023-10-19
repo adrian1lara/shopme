a89256577da43669b9740039d0fdffb5cb5b4ed9
adrian1lara <adrianlara203@gmail.com>
2023-10-19 17:55:40 -0500

    map and funcs of productos items

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const ProductList = ( { data, addToCart }) => {
+
+    const uniqueCategories = [...new Set(data.map((product) => product.category))];
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    }
+
+    const handleAddToCart = (product) => {
+        addToCart(product);
+    }
+
+    const filteredProducts = selectedCategory
+        ? data.filter((product) => product.category === selectedCategory)
+        : data;
+
+    return (
+        <>
+            <label htmlFor="category">Category</label>
+            <select
+                name="category"
+                id="category"
+                value={selectedCategory}
+                onChange={handleCategoryChange}
+            >
+                <option value="">All</option>
+                {uniqueCategories.map((category) => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
+
+            <div style={{ display: 'flex', flexWrap: 'wrap', }}>
+                {filteredProducts.map((product) => (
+                    <div key={product.id} style={{ width: '200px', margin: '10px' }}>
+                        <p>{product.title}</p>
+                        <img style={
+                            {
+                                    width: '200px',
+                                    height: '200px'
+                            }
+                        }
+                        src={product.image} alt={product.title} />
+                        <p>{product.price}</p>
+                        <p>{product.category}</p>
+                        <button onClick={() => handleAddToCart(product)}>Add to cart</button>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+ProductList.propTypes = {
+    data: PropTypes.array,
+    addToCart: PropTypes.func
+}
+
+export default ProductList
