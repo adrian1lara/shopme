216a4faeeec6f29e1db84e67fcf19c39e53cc8cb
adrian1lara <adrianlara203@gmail.com>
2023-10-20 00:12:31 -0500

    styling component with radix ui

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,18 @@
-import { Heading, Link } from "@radix-ui/themes"
+import { Box, Flex, Heading, Link } from "@radix-ui/themes"
 
 const Nav = () => {
     return (
         <>
-            <Heading as="h1">Shopme</Heading>
-            <nav>
-                <li>
-                    <Link href="/">Home</Link>
-                    <Link href="/products">Products</Link>
-                </li>
-            </nav>
+            <Flex justify={"space-between"} align={"center"}>
+                <Heading as="h1" size="xl" color="cyan">Shop<span style={{ color: "orange" }}>me</span></Heading>
+
+                <nav>
+                    <Box m="2" >
+                        <Link href="/" size="4" mr="2">Home</Link>
+                        <Link href="/products" size="4" mr="2">Products</Link>
+                    </Box>
+                </nav>
+            </Flex>
         </>
     )
 }
