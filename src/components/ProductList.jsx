b32792b66c9e78403bd85e4637789e22a427e038
adrian1lara <adrianlara203@gmail.com>
2023-10-20 00:13:14 -0500

    styling components with css and radix-ui

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@radix-ui/themes';
+import { Button, Card, Inset, Text, Box, Theme } from '@radix-ui/themes';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
@@ -21,37 +21,72 @@ const ProductList = ( { data, addToCart }) => {
 
     return (
         <>
-            <label htmlFor="category">Category</label>
-                    <select
-                        name="category"
-                        id="category"
-                        value={selectedCategory}
-                        onChange={handleCategoryChange}>
-                        <option value="">All</option>
-                        {uniqueCategories.map((category) => (
-                            <option key={category} value={category}>
-                                {category}
-                            </option>
-                        ))}
-            </select>
+        <Theme appearance="dark">
+            <Box style={{height: '100%'}} >
+                <Box style={{margin: '1rem', }}>
+                <label htmlFor="category"  style={{margin: '0.5rem' }}>CATEGORY</label>
+                        <select
+                            name="category"
+                            id="category"
+                            value={selectedCategory}
+                            onChange={handleCategoryChange}
+                            style={{
+                                padding: '0.5rem',
+                                borderRadius: '0.5rem',
+                                border: '1px solid #ccc',
+                                cursor: 'pointer',
+                                margin: '0.5rem',
+                                color: 'var(--gray-11)',
+                                fontWeight: 'bold',
+                                backgroundColor: 'var(--gray-5)',
+                                textTransform: 'uppercase',
+                            }}>
+                            <option value="" >All</option>
+                            {uniqueCategories.map((category) => (
+                                <option 
+                                key={category} 
+                                value={category} 
+                                style={{
+                                    color: 'var(--gray-11)',
+                                    fontWeight: 'bold',
+                                }}
+                                >
+                                    {category}
+                                </option>
+                            ))}
+                </select>
+                </Box>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap', }}>
-                {filteredProducts.map((product) => (
-                    <div key={product.id} style={{ width: '200px', margin: '10px' }}>
-                        <p>{product.title}</p>
-                        <img style={
-                            {
-                                    width: '200px',
-                                    height: '200px'
-                            }
-                        }
-                        src={product.image} alt={product.title} />
-                        <p>{product.price}</p>
-                        <p>{product.category}</p>
-                        <Button onClick={() => handleAddToCart(product)}>Add to cart</Button>
-                    </div>
-                ))}
-            </div>
+                <Box style={{ 
+                    display: 'flex', 
+                    flexWrap: 'wrap', 
+                    justifyContent: 'center',
+                    gap: '1rem',
+                    maxWidth:'1200px',
+                    margin: '0 auto' 
+                    }}>
+                    {filteredProducts.map((product) => (
+                        <Card key={product.id} size="2" style={{ width: '300px' }}>
+                            <Inset clip="padding-box" side="top" pb="current">
+                                <img style={
+                                    {
+                                        display: 'block',
+                                        objectFit: 'cover',
+                                        width: '100%',
+                                        height: 200,
+                                        backgroundColor: 'var(--gray-5)',
+                                    }
+                                }
+                                src={product.image} alt={product.title} />
+                            </Inset>
+                            <Text as='p'>{product.title}</Text>
+                            <Text as='p'>Price: ${product.price}</Text>
+                            <Button onClick={() => handleAddToCart(product)}>Add to cart</Button>
+                        </Card>
+                    ))}
+                </Box>
+            </Box>
+        </Theme>
         </>
     )
 }
