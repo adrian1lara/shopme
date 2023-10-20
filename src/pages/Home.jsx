0bafec2bd30d14a5d1f761fcd0eef77e8132ffc0
adrian1lara <adrianlara203@gmail.com>
2023-10-20 00:14:15 -0500

    added Box, Qoutes, Button and Heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Theme } from "@radix-ui/themes"
+import {Button, Theme, Box, Blockquote, Heading, Link, Quote } from "@radix-ui/themes"
 import Nav from "../components/Nav"
 
 const Home = () => {
@@ -7,8 +7,33 @@ const Home = () => {
         <>
             <Theme appearance="dark">
             <Nav />
-            <h1> Shopme Home </h1>
+                <Box style={{ display: 'flex', 
+                flexDirection: 'row', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                width: '100%',
+                height: '100vh'}}
+                >
+                    
+                    <Box width={'50%'}>
+                    <Heading as="h1" size="xl" color="orange"> Shopme Home </Heading>
+                    <Blockquote ml={''}>
+                        <Quote>Perfect typography is certainly the most elusive of all arts. Sculpture in
+                        stone alone comes near it in obstinacy.</Quote>
+                    </Blockquote>
+                        <Button 
+                            mt={"2"} 
+                            size={"3"} 
+                            color="cyan"
+                            variant="soft"
+                            asChild >
+                            <Link href="/products" underline="none"
+                            style={{borderRadius: '5px'}}>Come here</Link>
+                        </Button>
+                    </Box>
+                </Box>
             </Theme>
+            
         </>
     )
 }
