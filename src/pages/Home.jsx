911976e0c077eedd08a72e16670a8f504ef21df4
adrian1lara <adrianlara203@gmail.com>
2023-10-19 21:05:16 -0500

    added theme from rudix-ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { Theme } from "@radix-ui/themes"
 import Nav from "../components/Nav"
 
 const Home = () => {
 
     return (
         <>
+            <Theme appearance="dark">
             <Nav />
             <h1> Shopme Home </h1>
+            </Theme>
         </>
     )
 }
