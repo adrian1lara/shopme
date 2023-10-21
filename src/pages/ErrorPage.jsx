22ebde2c2d4f35f34e5c609a9edf1b03c91e61f9
adrian1lara <adrianlara203@gmail.com>
2023-10-21 00:10:27 -0500

    styling page error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,35 @@
+import { Box, Heading } from "@radix-ui/themes";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
+
     return (
         <>
-            <h1>404</h1>
-            <Link to="/">go back to home</Link>
+            <Box style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100%',
+            }}>
+                <Heading as="h1" style={ {
+                    color: 'var(--gray-12)',
+                    fontSize: '5rem',
+                    fontWeight: 'bold',
+                    margin: '1rem',
+                }} >
+                    Error 404
+                </Heading>
+                <Heading as="h2" style={{
+                    color: 'var(--gray-10)',
+                    fontSize: '2rem',
+                    fontWeight: 'bold',
+                    margin: '1rem',
+                }}>Page not found</Heading>
+                <Link to="/">go back to home</Link>
+            </Box>
+            
         </>
     )
 }
